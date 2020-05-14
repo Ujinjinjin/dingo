0274c1bad48d7844336fed187a46fcf330a82712
Camille Galladjov <ujinjinjin@outlook.com>
2020-05-14 23:32:26 +0300

    #1 few database connection utils

---
+++ Src/Dingo.Cli/Repository/PostgresDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Dingo.Cli.DbUtils;
+using Dingo.Cli.Loggers;
+using LinqToDB;
+using Microsoft.Extensions.Logging;
+
+namespace Dingo.Cli.Repository
+{
+	internal sealed class PostgresDbContext : DataConnectionBase
+	{
+		private static readonly ILogger Logger = new ConsoleLogger();
+		
+		protected internal PostgresDbContext(string connectionString)
+			: base(ProviderName.SqlServer2017, connectionString, Logger)
+		{
+		}
+	}
+}
