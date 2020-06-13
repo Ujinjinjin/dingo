eecf404e5ff50dbf52e78bca3d02af1d012c83e0
Camille Galladjov <ujinjinjin@outlook.com>
2020-06-13 21:43:27 +0300

    #8 service collection extensions

---
+++ Src/Dingo.Cli/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿﻿using Dingo.Cli.Config;
+ using Dingo.Cli.Factories;
+ using Dingo.Cli.Operations;
+ using Dingo.Cli.Repository;
+ using Dingo.Cli.Serializers;
+ using Microsoft.Extensions.DependencyInjection;
+
+ namespace Dingo.Cli.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static void AddDingoDependencies(this IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddSingleton<IConfigLoader, ConfigLoader>();
+			serviceCollection.AddSingleton<IConfigSaver, ConfigSaver>();
+			serviceCollection.AddSingleton<IConfigWrapper, ConfigWrapper>();
+			
+			serviceCollection.AddSingleton<IDatabaseContextFactory, DatabaseContextFactory>();
+			serviceCollection.AddSingleton<IInternalSerializerFactory, InternalSerializerFactory>();
+			
+			serviceCollection.AddSingleton<IDatabaseOperations, DatabaseOperations>();
+			serviceCollection.AddSingleton<IDirectoryScanner, DirectoryScanner>();
+			serviceCollection.AddSingleton<IHashMaker, HashMaker>();
+			serviceCollection.AddSingleton<IPathHelper, PathHelper>();
+			serviceCollection.AddSingleton<IProgramOperations, ProgramOperations>();
+			
+			serviceCollection.AddSingleton<IDatabaseContext, DatabaseContext>();
+		}
+	}
+}
