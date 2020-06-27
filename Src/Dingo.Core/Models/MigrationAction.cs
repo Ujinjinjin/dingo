5e4948ee25ef1a73cc0c67dc7ad908e18a1a8937
Camille Galladjov <ujinjinjin@outlook.com>
2020-06-27 11:39:31 +0300

    #13 additional doc strings

--- Src/Dingo.Core/Models/MigrationAction.cs
+++ Src/Dingo.Core/Models/MigrationAction.cs
@@ -2,9 +2,16 @@
 {
 	internal enum MigrationAction
 	{
+		/// <summary> Unknown action </summary>
 		Unknown = 0,
+
+		/// <summary> Migration is not aplied yet </summary>
 		Install = 1,
+
+		/// <summary> Older version of migration has been aplied earlier and can be updated </summary>
 		Update = 2,
+
+		/// <summary> Migration is up to date, so no actions are required </summary>
 		Skip = 3,
 	}
 }
--- Src/Dingo.Core/Models/MigrationInfo.cs
+++ Src/Dingo.Core/Models/MigrationInfo.cs
@@ -1,10 +1,18 @@
 ﻿namespace Dingo.Core.Models
 {
+	/// <summary> Info about migration file </summary>
 	internal struct MigrationInfo
 	{
+		/// <summary> Path to migration file </summary>
 		public FilePath Path { get; set; }
+		
+		/// <summary> Migration file hash </summary>
 		public string NewHash { get; set; }
+		
+		/// <summary> File's old migration hash (if it was applied earlier) </summary>
 		public string OldHash { get; set; }
+		
+		/// <summary> Action required to apply to migration </summary>
 		public MigrationAction Action { get; set; }
 	}
 }
