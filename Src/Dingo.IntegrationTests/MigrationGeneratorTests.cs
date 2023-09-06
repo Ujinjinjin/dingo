5771605c12596152615c1fe7c08c7edd41eb4425
Camille Galladjov <ujinjinjin@outlook.com>
2023-09-06 19:22:10 +0200

    #48 more migration generator tests

--- Src/Dingo.IntegrationTests/MigrationGeneratorTests.cs
+++ Src/Dingo.IntegrationTests/MigrationGeneratorTests.cs
@@ -1,3 +1,4 @@
+using AutoFixture;
 using Dingo.Core.Migrations;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,5 +29,29 @@ public class MigrationGeneratorTests : IntegrationTestBase
 		var migrationFilePath = migrationFiles.Single(x => x.Contains(filename));
 		var migrationContents = await File.ReadAllTextAsync(migrationFilePath);
 		migrationContents.Should().BeEmpty();
+	}
+
+	[Fact]
+	public async Task MigrationGeneratorTests_GenerateAsync__WhenNotExistingPathGiven_ThenDirectoryAndFileCreated()
+	{
+		// arrange
+		var generator = ServiceProvider.GetService<IMigrationGenerator>();
+		var timestamp = DateTime.UtcNow.Ticks;
+		var filename = $"{nameof(MigrationGeneratorTests)}{timestamp}";
+		var directory = Fixture.Create<string>();
+		var path = $"./temp/migrations/{directory}";
+
+		// act & assert
+		generator.Should().NotBeNull();
+		await generator.GenerateAsync(filename, path);
+		var dirExists = Directory.Exists(path);
+
+		// directory must exist
+		dirExists.Should().BeTrue();
+
+		// migration must be created
+		var migrationFiles = Directory.GetFiles(path);
+		migrationFiles.Should().NotBeEmpty();
+		migrationFiles.Should().ContainSingle(x => x.Contains(filename));
 	}
 }
