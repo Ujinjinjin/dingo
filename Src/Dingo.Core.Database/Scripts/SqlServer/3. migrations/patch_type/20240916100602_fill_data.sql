88f73720ee32ef85a73d1557322fe885a81004e2
Camille Galladjov <ujinjinjin@outlook.com>
2024-09-16 15:57:56 +0200

    fix: #90 improve fill data scripts safety

--- Src/Dingo.Core.Database/Scripts/SqlServer/3. migrations/patch_type/20240916100602_fill_data.sql
+++ Src/Dingo.Core.Database/Scripts/SqlServer/3. migrations/patch_type/20240916100602_fill_data.sql
@@ -1,7 +1,20 @@
 -- up
-insert into dingo.patch_type (patch_type_id, name) values
+drop table if exists #tt_source;
+select patch_type_id, name into #tt_source from dingo.patch_type where 1 = 0;
+insert into #tt_source (patch_type_id, name) values
 	(1, 'system'),
-	(2, 'user');
+	(2, 'user')
+;
+
+merge into dingo.patch_type as target
+using #tt_source as source
+	on source.patch_type_id = target.patch_type_id
+when not matched then
+	insert (patch_type_id, name) values (source.patch_type_id, source.name)
+when matched and target.name <> source.name then
+	update set name = source.name
+when not matched by source then delete
+;
 
 -- down
 delete from dingo.patch_type where patch_type_id in (1, 2);
